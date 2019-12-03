0958ccd736fa1fd4e85551057a7192dee08d092f
Emil Aminy <emilaminy@outlook.com>
2019-12-03 20:29:06 +0100

    changed pickle to json

--- emil_petter/src/hgnc.py
+++ emil_petter/src/hgnc.py
@@ -1,38 +1,36 @@
 import json
-import pickle
-from Protein import Protein
 
-hgnc = json.load(open("hgnc_complete_set.json"))
-counter = 0
-proteins = []
+hgnc = json.load(open("../data/hgnc_complete_set.json"))
+# counter = 0
+proteins = {}
 for entry in hgnc["response"]["docs"]:
-    prot = Protein()
+    prot = {"altNames": []}
     
     if "symbol" in entry:
-        prot.name = entry["symbol"]
+        prot["name"] = entry["symbol"]
     
     if "alias_name" in entry:
-        prot.altNames += entry["alias_name"]
+        prot["altNames"] += entry["alias_name"]
 
     if "alias_symbol" in entry:
-        prot.altNames += entry["alias_symbol"]
+        prot["altNames"] += entry["alias_symbol"]
     
     if "ensembl_gene_id" in entry:
-        prot.ensemblID["gene"] = entry["ensembl_gene_id"]
+        prot["ensemblGeneID"] = entry["ensembl_gene_id"]
 
     if "hgnc_id" in entry:
-        prot.hgncID = entry["hgnc_id"]
+        prot["hgncID"] = entry["hgnc_id"]
     
     if "uniprot_ids" in entry:
-        prot.uniprotID = entry["uniprot_ids"][0]
+        prot["uniprotID"] = entry["uniprot_ids"][0]
     
     if "entrez_id" in entry:
-        prot.uniprotID = entry["entrez_id"]
+        prot["entrezID"] = entry["entrez_id"]
     
-    prot.speciesID = "9606"
-    prot.speciesName = "Homo sapiens"
-    prot.ID = prot.ID = "LUGE00009606" + "{:08d}".format(counter)
-    counter += 1
-    proteins.append(prot)
+    prot["speciesID"] = "9606"
+    prot["speciesName"] = "Homo sapiens"
+    # ID = "LUGE00009606" + "{:08d}".format(counter)
+    # counter += 1
+    proteins[prot["hgncID"]] = prot
 
-pickle.dump(proteins, open("hgnc.out", "wb"))
+json.dump(proteins, open("hgnc.json", "w+"))
--- emil_petter/src/uniprot.py
+++ emil_petter/src/uniprot.py
@@ -1,19 +1,18 @@
-import pickle
+import json
 from lxml import etree
-from Protein import Protein
 
-counter = 0
+# counter = 0
 prefix = "{http://uniprot.org/uniprot}"
 
-proteins = []
+proteins = {}
 
-for event, element in etree.iterparse("uniprot_sprot.xml", tag=prefix + "entry"):
-    prot = Protein()
+for event, element in etree.iterparse("../data/uniprot_sprot.xml", tag=prefix + "entry"):
+    prot = {"altNames": []}
     for child in element.getchildren():
 
         # Gets uniprot ID
-        if child.tag == prefix + "accession" and prot.uniprotID is None:
-            prot.uniprotID = child.text
+        if child.tag == prefix + "accession" and "uniprotID" not in prot:
+            prot["uniprotID"] = child.text
 
         # Gets alternative names
         elif child.tag.endswith("protein"):
@@ -21,7 +20,7 @@ for event, element in etree.iterparse("uniprot_sprot.xml", tag=prefix + "entry")
 
                 if entry.tag == prefix + "recommendedName" or entry.tag == prefix + "alternativeName":
                     for name in entry.getchildren():
-                        prot.altNames.append(name.text)
+                        prot["altNames"].append(name.text)
 
         # Gets name and more alternative names
         elif child.tag == prefix + "gene":
@@ -29,43 +28,42 @@ for event, element in etree.iterparse("uniprot_sprot.xml", tag=prefix + "entry")
 
                 if entry.tag == prefix + "name":
                     if entry.attrib["type"] == "primary":
-                        prot.name = entry.text
+                        prot["name"] = entry.text
                     elif entry.attrib["type"] == "synonym":
-                        prot.altNames.append(entry.text)
+                        prot["altNames"].append(entry.text)
 
         # Gets species name and ID
         elif child.tag == prefix + "organism":
             for entry in child.getchildren():
 
                 if entry.tag == prefix + "name" and entry.attrib["type"] == "scientific":
-                    prot.speciesName = entry.text
+                    prot["speciesName"] = entry.text
                 elif entry.tag == prefix + "dbReference" and entry.attrib["type"] == "NCBI Taxonomy":
-                    prot.speciesID = entry.attrib["id"]
+                    prot["speciesID"] = entry.attrib["id"]
 
         # Gets hgnc ID
         elif child.tag == prefix + "dbReference" and child.attrib["type"] == "HGNC":
-            prot.hgncID = child.attrib["id"]
+            prot["hgncID"] = child.attrib["id"]
 
         # Gets ensembl IDs
         elif child.tag == prefix + "dbReference" and child.attrib["type"] == "Ensembl":
-            prot.ensemblID["transcript"] = child.attrib["id"]
+            prot["ensemblTranscriptID"] = child.attrib["id"]
             for entry in child.getchildren():
 
                 if entry.tag == "property" and entry.attrib["type"] == "protein sequence ID":
-                    prot.ensemblID["protein"] = child.attrib["value"]
+                    prot["ensemblPropertyID"] = child.attrib["value"]
                 elif entry.tag == "property" and entry.attrib["type"] == "gene ID":
-                    prot.ensemblID["gene"] = child.attrib["value"]
+                    prot["ensemblGeneID"] = child.attrib["value"]
 
-    if prot.speciesID is not None:
-        prot.ID = "LUGE" + "{:08d}".format(int(prot.speciesID)) + "{:08d}".format(counter)
-    else:
-        prot.ID = "LUGE00000000" + "{:08d}".format(counter)
+    # ID = ""
+    # if prot["speciesID"] is not None:
+    #     ID = "LUGE" + "{:08d}".format(int(prot["speciesID"])) + "{:08d}".format(counter)
+    # else:
+    #     ID = "LUGE00000000" + "{:08d}".format(counter)
+    # counter += 1
+    # proteins[ID] = prot
 
-    counter += 1
-
-    proteins.append(prot)
+    proteins[prot["uniprotID"]] = prot
     element.clear()
-    if counter % 1000 == 0:
-        print(counter)
 
-pickle.dump(proteins, open("uniprot.out", "wb"))
+json.dump(proteins, open("uniprot.json", "w+"))
