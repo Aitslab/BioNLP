e4504b05c6dc95c70bc76330b44b0ef01e1d6fcb
OlaOlde <ola.olde@hotmail.com>
2021-04-26 16:26:31 +0200

    small update of file, adding comments

--- Adam_Ola/formatInputFile.py
+++ Adam_Ola/formatInputFile.py
@@ -1,15 +1,14 @@
-# This is a sample Python script.
+# This is a python script to convert Hunflair corpora to the same format as that of biobert input.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import csv
-
 import re
+
 plus = r"( O \+)"
 minus = r"( O \-)"
 b = r"(B-[A-Za-z]+ [\+ \-]?)"
 isub = r"(I-[A-Za-z]+ [\+ \-]?)"
 
+#Takes a string as input and change to the IOB format
 def convert_iob(t):
     t = re.sub(plus,  "\tO", t)
     t = re.sub(minus, "\tO", t)
@@ -17,10 +16,12 @@ def convert_iob(t):
     t = re.sub(isub,  "\tI", t)
     return t
 
+#Converts string to list
 def convert_to_list(string):
     li = list(string.split())
     return li
 
+#Converts list to tsv
 def list_to_tsv(list_to_convert):
     with open('output.tsv', 'w', newline='') as f_output:
         tsv_output = csv.writer(f_output, delimiter='\t')
