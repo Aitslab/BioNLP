d2e201cad74c96e46cd3e735f54c520f28615d7d
Klara Broman <klarabroman@live.se>
2021-04-27 14:07:57 +0200

    bert finetune that saves on every epoch, updated metrics script

--- lykke_klara/relation_extraction/metrics.py
+++ lykke_klara/relation_extraction/metrics.py
@@ -5,47 +5,48 @@ from transformers import BertTokenizer
 from transformers import BertForSequenceClassification, AdamW, BertConfig
 from sklearn.metrics import confusion_matrix
 
-input_dir = '/content/drive/MyDrive/EDAN70/bert-finetuned'
-train_file = open("/content/drive/MyDrive/EDAN70/datasets/train.txt", "r")
-dev_file = open("/content/drive/MyDrive/EDAN70/datasets/dev.txt", "r")
+for i in range(3):
+  input_dir = '/content/drive/MyDrive/EDAN70/bert-finetuned-{}'.format(i)
+  #train_file = open("/content/drive/MyDrive/EDAN70/custom_label_datasets/train.txt", "r")
+  dev_file = open("/content/drive/MyDrive/EDAN70/custom_label_datasets/dev.txt", "r")
 
-train_list = train_file.read().split("\n")
-dev_list = dev_file.read().split("\n")
+  # train_list = train_file.read().split("\n")
+  dev_list = dev_file.read().split("\n")
 
-if torch.cuda.is_available():
+  if torch.cuda.is_available():
 
-    device = torch.device("cuda")
+      device = torch.device("cuda")
 
-    print('\n\nFound %d available GPU(s).' % torch.cuda.device_count())
-    print('Using GPU:', torch.cuda.get_device_name(0))
+      print('\n\nFound %d available GPU(s).' % torch.cuda.device_count())
+      print('Using GPU:', torch.cuda.get_device_name(0))
 
-else:
-    print("\n\nNo GPU(s) available, switching to CPU.")
-    device = torch.device("cpu")
+  else:
+      print("\n\nNo GPU(s) available, switching to CPU.")
+      device = torch.device("cpu")
 
-# to load the model later:
-model = BertForSequenceClassification.from_pretrained(input_dir, local_files_only=True, cache_dir=None)
-tokenizer = BertTokenizer.from_pretrained(input_dir)
-model.to(device)
+  # to load the model later:
+  model = BertForSequenceClassification.from_pretrained(input_dir, local_files_only=True, cache_dir=None)
+  tokenizer = BertTokenizer.from_pretrained(input_dir)
+  model.to(device)
 
-classes = ["NOT", "PART-OF", "INTERACTOR", "REGULATOR-POSITIVE", "REGULATOR-NEGATIVE", "OTHER"]
+  classes = ["NOT", "PART-OF", "INTERACTOR", "REGULATOR-POSITIVE", "REGULATOR-NEGATIVE", "OTHER"]
 
-predictions = []
-correct_predictions = 0
+  predictions = []
+  correct_predictions = 0
 
-for seq in train_list:
-  #print(train_list.index(seq), "\n")
-  text = json.loads(seq)["text"]
-  actual_class = json.loads(seq)["custom_label"]
-  input_ids = torch.tensor(tokenizer.encode(text)).unsqueeze(0).to(device)
-  outputs = model(input_ids)
-  pred_class = classes[torch.softmax(outputs.logits, dim=1).argmax()]
+  for seq in dev_list:
+    #print(train_list.index(seq), "\n")
+    text = json.loads(seq)["text"]
+    actual_class = json.loads(seq)["custom_label"]
+    input_ids = torch.tensor(tokenizer.encode(text)).unsqueeze(0).to(device)
+    outputs = model(input_ids)
+    pred_class = classes[torch.softmax(outputs.logits, dim=1).argmax()]
 
-  if (pred_class == actual_class):
-    correct_predictions += 1
-  
-  predictions.append(pred_class)
+    if (pred_class == actual_class):
+      correct_predictions += 1
+    
+    predictions.append(pred_class)
 
-# # predict on the chemprot training and dev sets
-print("Accuracy: ", correct_predictions / len(train_list))
-# #print("Accuracy: ", correct_predictions / len(dev_list)) # Accuracy:  0.5174825174825175
+  # # predict on the chemprot training and dev sets
+  print("Accuracy: ", correct_predictions / len(train_list))
+  # #print("Accuracy: ", correct_predictions / len(dev_list)) # Accuracy:  0.5174825174825175
