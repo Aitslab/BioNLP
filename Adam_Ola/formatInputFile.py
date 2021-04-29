43ff521ec107df83b6cc8e6bb35ee30dc65da93a
OlaOlde <ola.olde@hotmail.com>
2021-04-29 17:07:40 +0200

    update of script

--- Adam_Ola/formatInputFile.py
+++ Adam_Ola/formatInputFile.py
@@ -7,6 +7,7 @@ plus = r"( O \+)"
 minus = r"( O \-)"
 b = r"(B-[A-Za-z]+ [\+ \-]?)"
 isub = r"(I-[A-Za-z]+ [\+ \-]?)"
+newl = r"(\n\n)"
 
 #Takes a string as input and change to the IOB format
 def convert_iob(t):
@@ -14,6 +15,7 @@ def convert_iob(t):
     t = re.sub(minus, "\tO", t)
     t = re.sub(b, "\tB", t)
     t = re.sub(isub,  "\tI", t)
+    t = re.sub(newl, "\tnewLine2000\tnewLine2000\t", t)
     return t
 
 #Converts string to list
@@ -23,15 +25,21 @@ def convert_to_list(string):
 
 #Converts list to tsv
 def list_to_tsv(list_to_convert):
-    with open('output.tsv', 'w', newline='') as f_output:
+    with open('test.tsv', 'w', newline='') as f_output:
         tsv_output = csv.writer(f_output, delimiter='\t')
+        s = ''
         for i in range(0,len(list_to_convert),2):
-            tsv_output.writerow([list_to_convert[i] , list_to_convert[i+1]])
+            if list_to_convert[i] == 'newLine2000':
+                tsv_output.writerow(s)
+            else:
+                tsv_output.writerow([list_to_convert[i], list_to_convert[i + 1]])
 
 
 
-c = open("SciSpacySentenceSplitter_core_sci_sm_0.2.5_SciSpacyTokenizer_core_sci_sm_0.2.5_dev.conll", "r")
+
+c = open("TagSentenceSplitter_[__SENT__]_SciSpacyTokenizer_core_sci_sm_0.2.5_test.conll", "r")
 text = c.read()
 text = convert_iob(text)
 l = convert_to_list(text)
+print(l[0:100])
 list_to_tsv(l)
