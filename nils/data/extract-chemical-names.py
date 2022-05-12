41d8c9c64ec319e41c265b847a76736d8663c2fc
NilsCarlFredrik <nils.broman@gmail.com>
2022-05-12 14:30:44 +0200

    updated extrach-chemical-names.py to not include duplicates

--- nils/data/extract-chemical-names.py
+++ nils/data/extract-chemical-names.py
@@ -4,15 +4,15 @@ import json
 entities_file_path = "chemprot/chemprot_training/chemprot_training_entities.tsv"
 output_path = "artificial-building-blocks/element2_4_chemical_names_chemprot.txt"
 
-chem_list = []
+chem_set = set()
 
 # saves all entities marked as chemicals to list
 with open(entities_file_path, encoding="utf8") as file:
     tsv_file = csv.reader(file, delimiter="\t")
     for line in tsv_file:
-        if line[2] == "CHEMICAL": chem_list.append(line[-1])
+        if line[2] == "CHEMICAL": chem_set.add(line[-1])
 
 # creates a new file with all chemicals
 with open(output_path, 'x', encoding="utf8") as outfile:
-    for item in chem_list:
+    for item in chem_set:
         outfile.write(item + "\n")
